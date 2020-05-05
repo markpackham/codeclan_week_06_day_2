3929d89cf53ae14423a40212ac08b757179df6ac
mark <markpackham1@gmail.com>
2020-05-05 17:13:43 +0100

    Working on Add Dino

--- park.js
+++ park.js
@@ -4,4 +4,16 @@ const Park = function (name, price) {
   this.dinosaursCollected = [];
 };
 
+Park.prototype.addDinosaur = function () {
+  this.dinosaursCollected.push;
+};
+
+Park.prototype.removeDinosaur = function (name) {
+  const indexOfDino = this.dinosaursCollected.indexOf(name);
+  this.dinosaursCollected.splice(indexOfDino, 1);
+};
+
 module.exports = Park;
+
+// codeclan_week_06_day_2
+// CodeClan Week 06 Day 2 Jurassic Park
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -4,8 +4,12 @@ const Dinosaur = require("../dinosaur.js");
 
 describe("Park", function () {
   let park;
+  let dinosaur1;
+  let dinosaur2;
   beforeEach(function () {
     park = new Park("Yellow Stone", 100);
+    dinosaur1 = new Dinosaur("t-rex", "carnivore", 50);
+    dinosaur2 = new Dinosaur("raptor", "carnivore", 10);
   });
 
   it("should have a name", function () {
@@ -29,7 +33,14 @@ describe("Park", function () {
     assert.deepStrictEqual(actual, ["Stegasaurus"]);
   });
 
-  it("should be able to remove a dinosaur from its collection");
+  it("should be able to remove a dinosaur from its collection", function () {
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.removeDinosaur(dinosaur1);
+    const expected = dinosaur2;
+    const actual = park.dinosaursCollected;
+    assert.deepStrictEqual(actual, expected);
+  });
 
   it("should be able to find the dinosaur that attracts the most visitors");
 
