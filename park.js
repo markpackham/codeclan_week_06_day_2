1d446235472d06cac181916f3d07c9d45841624b
mark <markpackham1@gmail.com>
2020-05-05 16:54:38 +0100

    Working Adding Dinosaur

---
+++ park.js
@@ -0,0 +1,7 @@
+const Park = function (name, price) {
+  this.name = name;
+  this.price = price;
+  this.dinosaursCollected = [];
+};
+
+module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,31 +1,43 @@
-const assert = require('assert');
-const Park = require('../park.js');
-const Dinosaur = require('../dinosaur.js');
+const assert = require("assert");
+const Park = require("../park.js");
+const Dinosaur = require("../dinosaur.js");
 
-describe('Park', function() {
-
+describe("Park", function () {
+  let park;
   beforeEach(function () {
+    park = new Park("Yellow Stone", 100);
+  });
 
-  })
+  it("should have a name", function () {
+    const actual = park.name;
+    assert.strictEqual(actual, "Yellow Stone");
+  });
 
-  it('should have a name');
+  it("should have a ticket price", function () {
+    const actual = park.price;
+    assert.strictEqual(actual, 100);
+  });
 
-  it('should have a ticket price');
+  it("should have a collection of dinosaurs", function () {
+    const actual = park.dinosaursCollected;
+    assert.deepStrictEqual(actual, []);
+  });
 
-  it('should have a collection of dinosaurs');
+  it("should be able to add a dinosaur to its collection", function () {
+    park.addDinosaur("Stegasaurus");
+    const actual = park.dinosaursCollected;
+    assert.deepStrictEqual(actual, ["Stegasaurus"]);
+  });
 
-  it('should be able to add a dinosaur to its collection');
+  it("should be able to remove a dinosaur from its collection");
 
-  it('should be able to remove a dinosaur from its collection');
+  it("should be able to find the dinosaur that attracts the most visitors");
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it("should be able to find all dinosaurs of a particular species");
 
-  it('should be able to find all dinosaurs of a particular species');
+  it("should be able to calculate the total number of visitors per day");
 
-  it('should be able to calculate the total number of visitors per day');
+  it("should be able to calculate the total number of visitors per year");
 
-  it('should be able to calculate the total number of visitors per year');
-
-  it('should be able to calculate total revenue for one year');
-
+  it("should be able to calculate total revenue for one year");
 });
